595e87e5e972720d334069e5fee0bd0cc52dd7ff
Musfida Irawan Putri <musfidaxiaomi112@gmail.com>
2025-06-04 11:39:09 +0700

    revisi tabel kategori produk

--- admin/kategori.php
+++ admin/kategori.php
@@ -200,7 +200,11 @@ exit;
         </div>
     </div>
     
-
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
               <!-- Table with stripped rows -->
               <table class="table table-striped mt-2">
                 <thead>
